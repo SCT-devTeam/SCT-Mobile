635cd2d526c6f24b4b775cef8c483509102f309d
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-02-11 23:38:39 +0100

    Onboarding creation
    - Added SVG illustration assets to the Flutter project
    - Created Onboarding page
    - Integrated style chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:sct_mobile/screens/onboarding_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new SCT(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SCT extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<SCT> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Color(0xfff7c91e),
-        accentColor: Color(0xfff7c91e),
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      title: 'Onboarding',
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
     );
   }
 }
