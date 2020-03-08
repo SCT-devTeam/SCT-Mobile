ec3528a3207875a23a9391353f20ceb217d8ad80
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-03-08 01:26:49 +0100

    Project reorganization & animated onboarding
    - Reorganization of the Flutter project files
    - Animated color transitions between pages
    - Temporary white and black alternatives for certain styles
    - Added provider dependency to pubspec.yaml

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+// TODO: Remove the white and black alternatives and create a theme for more appropriate use
+
+// HEADER LARGE BLACK
+final sctTitleStyleLarge_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Roboto',
+  fontSize: 45.0,
+  height: 1.5,
+  fontWeight: FontWeight.bold,
+);
+
+// HEADER LARGE WHITE
+final sctTitleStyleLarge_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Roboto',
+  fontSize: 45.0,
+  height: 1.5,
+  fontWeight: FontWeight.bold,
+);
+
+// HEADER MEDIUM BLACK
+final sctTitleStyleMed_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Roboto',
+  fontSize: 35.0,
+  height: 1.5,
+  fontWeight: FontWeight.bold,
+);
+
+// HEADER MEDIUM WHITE
+final sctTitleStyleMed_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Roboto',
+  fontSize: 35.0,
+  height: 1.5,
+  fontWeight: FontWeight.bold,
+);
+
+// TEXT
+final sctText = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana',
+  fontSize: 18.0,
+  height: 1.2,
+);
+
+// SUBTITLE LARGE BLACK
+final sctSubtitleStyleLarge_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana Pro',
+  fontSize: 24.0,
+  height: 1.2,
+);
+
+// SUBTITLE LARGE WHITE
+final sctSubtitleStyleLarge_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Verdana Pro',
+  fontSize: 24.0,
+  height: 1.2,
+);
+
+// SUBTITLE MEDIUM BLACK
+final sctSubtitleStyleMed_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana Pro',
+  fontSize: 20.0,
+  height: 1.2,
+);
+
+// SUBTITLE MEDIUM WHITE
+final sctSubtitleStyleMed_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Verdana Pro',
+  fontSize: 20.0,
+  height: 1.2,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sct_mobile/screens/onboarding_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:sct_mobile/ui/screens/onboarding_screen.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -7,18 +8,53 @@ void main() {
   ));
 }
 
-class SCT extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
+//class SCT extends StatefulWidget {
+////  @override
+////  _MyAppState createState() => new _MyAppState();
+////}
 
-class _MyAppState extends State<SCT> {
+class SCT extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+//    return Provider<AuthenticationProvider>(
+//        builder: (_) => AuthenticationProvider(),
+//    dispose: (_, model) => model.dispose(),
+//    child: Consumer<AuthenticationProvider>(
+//    builder: (_, AuthenticationProvider authenticationProvider, __) {
+//    return MultiProvider(
+//    providers: <SingleChildCloneableWidget>[
+//    Provider<ChatProvider>(
+//    builder: (_) => ChatProvider(authenticationProvider: authenticationProvider),
+//    dispose: (_, ChatProvider provider) => provider.dispose(),
+//    ),
+//    Provider<ResourcesProvider>(
+//    builder: (_) => ResourcesProvider(authenticationProvider: authenticationProvider),
+//    dispose: (_, ResourcesProvider provider) => provider.dispose(),
+//    ),
+//    ],
+
     return MaterialApp(
-      title: 'Onboarding',
       debugShowCheckedModeBanner: false,
+      title: 'SCT App',
+      theme: ThemeData(
+          // TODO: Create a theme for the app, to easily use different styles
+          ),
       home: OnboardingScreen(),
+      // TODO: Make the Onboarding screen appear only once after installation
     );
   }
 }
+//class _MyAppState extends State<SCT> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Onboarding',
+//      debugShowCheckedModeBanner: false,
+//      home: OnboardingScreen(),
+//    );
+//  }
+//}
