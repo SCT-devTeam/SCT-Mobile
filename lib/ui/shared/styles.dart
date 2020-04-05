16ea27be6f9d7697e1e5c594f937575bc2464b56
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-05 16:10:58 +0200

    Updated styles.dart to make use of SizeConfig

---
+++ lib/ui/shared/styles.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:sct_mobile/ui/shared/sizeConfig.dart';
+// TODO: Remove the white and black alternatives and create a theme for more appropriate use
+
+// HEADER LARGE BLACK
+final sctTitleStyleLarge_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Roboto',
+  fontSize: 50.0 * SizeConfig.textMultiplier,
+  height: 1.5,
+//  fontWeight: FontWeight.bold,
+);
+
+// HEADER LARGE WHITE
+final sctTitleStyleLarge_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Roboto',
+  fontSize: 50.0 * SizeConfig.textMultiplier,
+  height: 1.5,
+);
+
+// HEADER MEDIUM BLACK
+final sctTitleStyleMed_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Roboto',
+  fontSize: 35.0 * SizeConfig.textMultiplier,
+  height: 1.5,
+);
+
+// HEADER MEDIUM WHITE
+final sctTitleStyleMed_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Roboto',
+  fontSize: 35.0 * SizeConfig.textMultiplier,
+  height: 1.5,
+);
+
+// TEXT
+final sctText = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana',
+  fontSize: 18.0 * SizeConfig.textMultiplier,
+  height: 1.2,
+);
+
+// SUBTITLE LARGE BLACK
+final sctSubtitleStyleLarge_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana Pro',
+  fontSize: 24.0 * SizeConfig.textMultiplier,
+  height: 1.2,
+);
+
+// SUBTITLE LARGE WHITE
+final sctSubtitleStyleLarge_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Verdana Pro',
+  fontSize: 24.0 * SizeConfig.textMultiplier,
+  height: 1.2,
+);
+
+// SUBTITLE MEDIUM BLACK
+final sctSubtitleStyleMed_b = TextStyle(
+  color: Color(0xff1a1a1a),
+  fontFamily: 'Verdana Pro',
+  fontSize: 20.0 * SizeConfig.textMultiplier,
+  height: 1.2,
+);
+
+// SUBTITLE MEDIUM WHITE
+final sctSubtitleStyleMed_w = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Verdana Pro',
+  fontSize: 20.0 * SizeConfig.textMultiplier,
+  height: 1.2,
+);
