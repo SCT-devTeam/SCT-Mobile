4436d38c4e56f1a682ced4630456eb8a5c9ea095
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-09 19:36:17 +0200

    Designed the invoices page

--- lib/ui/screens/invoices.dart
+++ lib/ui/screens/invoices.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:sct_mobile/ui/widgets/invoicecard.dart';
+
+// Écritures
 
 class Invoices extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Stack(
-        children: <Widget>[
-          Center(child: Text("Page des écritures")),
-        ],
+    return WillPopScope(
+      child: Material(
+        child: ListView(
+          padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            InvoiceCard(
+                title: 'Fournitures',
+                icon: 'invoice_icon_black',
+                price: 350,
+                date: "06/03/1998",
+                color: 0xfffbe48e),
+          ],
+        ),
       ),
+      onWillPop: () async => false,
     );
   }
 }
