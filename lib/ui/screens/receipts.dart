d69695f1cff7742b1cd329dcdc6fa3879b46c525
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-09 19:36:26 +0200

    Designed the receipts page

--- lib/ui/screens/receipts.dart
+++ lib/ui/screens/receipts.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:sct_mobile/ui/widgets/personcard.dart';
+
+// Recettes
 
 class Receipts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Stack(
-        children: <Widget>[
-          Center(child: Text("Page des recettes")),
-        ],
+    return WillPopScope(
+      child: Material(
+        child: ListView(
+          padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            PersonCard(
+                title: 'John Doe\n Facture n°',
+                icon: 'avatar_icon_black',
+                value: 0660032567,
+                color: 0xffdcd2fd),
+          ],
+        ),
       ),
+      onWillPop: () async => false,
     );
   }
 }
