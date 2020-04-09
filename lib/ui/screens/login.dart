192c6dee5db92541a818ba5d6296229bd6a979fd
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-09 13:53:11 +0200

    Created the login form with implementation of login function connecting to the API

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,13 +1,87 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:sct_mobile/ui/shared/styles.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sct_mobile/ui/screens/scanner.dart';
+import 'package:sct_mobile/ui/screens/menu.dart';
 import 'package:sct_mobile/ui/widgets/primarybutton.dart';
-import 'package:sct_mobile/ui/widgets/primaryinput.dart';
+import 'package:validate/validate.dart';
+import 'package:sct_mobile/core/data/models/api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() => new _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+  bool _isLoading = false;
+  bool _connectButtonState = true;
+  bool _wrongCredentials = false;
+  ScaffoldState scaffoldState;
+
+  var _urlController = TextEditingController();
+  var _mailController = TextEditingController();
+  var _passwordController = TextEditingController();
+
+  final FocusNode _urlFocus = FocusNode();
+  final FocusNode _mailFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+
+//  _showMsg(msg) {
+//    //
+//    print(msg);
+//    final snackBar = SnackBar(
+//      content: Text(msg),
+//      action: SnackBarAction(
+//        label: 'Close',
+//        onPressed: () {
+//          // Some code to undo the change!
+//        },
+//      ),
+//    );
+//    Scaffold.of(context).showSnackBar(snackBar);
+//  }
+
+  String _validateEmail(String value) {
+    setState(() {
+      _wrongCredentials = false;
+    });
+    try {
+      Validate.isEmail(value);
+    } catch (e) {
+      return 'L\'adresse email n\'est pas valide.';
+    }
+    return null;
+  }
+
+  String _validatePassword(String value) {
+    setState(() {
+      _wrongCredentials = false;
+    });
+    if (value.isEmpty) {
+      return 'Veuillez renseigner le mot de passe';
+    }
+    return null;
+  }
+
+  void submit() {
+    if (this._formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      _login();
+    }
+  }
+
   Widget build(BuildContext context) {
+    _fieldFocusChange(
+        BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+      currentFocus.unfocus();
+      FocusScope.of(context).requestFocus(nextFocus);
+    }
+
     return MaterialApp(
       title: 'Welcome to SCT',
       home: Center(
@@ -22,7 +96,8 @@ class Login extends StatelessWidget {
             ),
           ),
           backgroundColor: Color(0xfff7c91e),
-          body: Container(
+          body: Form(
+            key: this._formKey,
             child: SingleChildScrollView(
               padding: EdgeInsets.all(30.0),
 //              mainAxisAlignment: MainAxisAlignment.,
@@ -31,24 +106,108 @@ class Login extends StatelessWidget {
                   SvgPicture.asset(
                       'assets/images/sct_logo_colored_squaredWithoutBackgroundAndSubtitle.svg'),
                   SizedBox(height: 40.0),
-                  PrimaryInput(title: "URL de l'instance"),
-                  PrimaryInput(title: "Adresse email"),
-                  PrimaryInput(title: "Mot de passe"),
+                  Text(
+                      _wrongCredentials
+                          ? 'Mauvais identifiant / mot de passe'
+                          : '',
+                      style: TextStyle(color: Colors.red)),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    controller: _urlController,
+                    autovalidate: true,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.url,
+                    focusNode: _urlFocus,
+                    onFieldSubmitted: (term) {
+                      _fieldFocusChange(context, _urlFocus, _mailFocus);
+                    },
+                    scrollPadding: EdgeInsets.all(30.0),
+                    decoration: InputDecoration(
+                      hintText: "URL de l'instance",
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.white, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      fillColor: const Color(0xffffffff).withOpacity(0.3),
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    controller: _mailController,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: this._validateEmail,
+                    focusNode: _mailFocus,
+                    onFieldSubmitted: (term) {
+                      _fieldFocusChange(context, _mailFocus, _passwordFocus);
+                    },
+                    scrollPadding: EdgeInsets.all(30.0),
+                    decoration: InputDecoration(
+                      hintText: "Adresse email",
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.white, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      fillColor: const Color(0xffffffff).withOpacity(0.3),
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    textInputAction: TextInputAction.done,
+                    obscureText: true,
+                    focusNode: _passwordFocus,
+                    onFieldSubmitted: (value) {
+                      _passwordFocus.unfocus();
+//                      submit();
+//                      _calculator();
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: this._validatePassword,
+                    scrollPadding: EdgeInsets.all(30.0),
+                    decoration: InputDecoration(
+                      hintText: "Mot de passe",
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.white, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 3.0),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      fillColor: const Color(0xffffffff).withOpacity(0.3),
+                      filled: true,
+                    ),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       SizedBox(width: 200.0),
                       PrimaryButton(
-                        title: Text("Se connecter",
-                            style: TextStyle(
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17)),
-                        onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Scanner()),
-                        ),
-                      ),
+                          title: Text(
+                              _isLoading ? 'Connexion...' : 'Se connecter',
+                              style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17)),
+                          isEnabled: _connectButtonState,
+                          onPressed: _isLoading ? null : submit),
                     ],
                   ),
                   PrimaryButton(
@@ -57,6 +216,7 @@ class Login extends StatelessWidget {
                             fontFamily: 'Roboto',
                             fontWeight: FontWeight.w500,
                             fontSize: 17)),
+                    isEnabled: true,
                     onPressed: () => Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => Scanner()),
@@ -69,5 +229,38 @@ class Login extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _login() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    var data = {
+      'email': _mailController.text,
+      'password': _passwordController.text,
+      'device_name': 'Mobile App'
+    };
+
+    var res = await CallApi().postData(data, 'api/airlock/login');
+    if (res.statusCode == 200) {
+      var body = json.decode(res.body);
+      if (body.containsKey('token')) {
+        print('Login success');
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('token', body['token']);
+//      prefs.setString('user', json.encode(body['user']));
+        Navigator.of(context).pushNamed('menu');
+      } else {
+        print("Error: No token found");
+      }
+    } else {
+      print("Error status code: ${res.statusCode}");
+      setState(() {
+        _isLoading = false;
+        _connectButtonState = true;
+        _wrongCredentials = true;
+      });
+    }
   }
 }
