cfb9a5233065d18ef873a1b96cbf7fce5ceac851
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-04-09 13:56:12 +0200

    Added login verification preference and redirection when the user opens the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,44 @@ import 'package:flutter/services.dart';
 import 'package:sct_mobile/ui/screens/onboarding_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sct_mobile/ui/screens/home.dart';
+import 'package:sct_mobile/ui/screens/menu.dart';
 
 int initScreen;
+bool _isLoggedIn = false;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
+  print('initScreen ${initScreen}');
+
   initScreen = await prefs.getInt("initScreen");
   await prefs.setInt("initScreen", 1);
-  print('initScreen ${initScreen}');
   runApp(SCT());
 }
 
-class SCT extends StatelessWidget {
+class SCT extends StatefulWidget {
+  @override
+  _SCTState createState() => _SCTState();
+}
+
+class _SCTState extends State<SCT> {
+  @override
+  void initState() {
+    _checkIfLoggedIn();
+    super.initState();
+  }
+
+  void _checkIfLoggedIn() async {
+    // check if token is there
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString('token');
+    if (token != null) {
+      setState(() {
+        _isLoggedIn = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -23,16 +48,21 @@ class SCT extends StatelessWidget {
     ]);
 
     return MaterialApp(
+      home: Scaffold(
+        body: _isLoggedIn ? Menu() : Home(),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'SCT App',
       theme: ThemeData(
           // TODO: Create a theme for the app, to easily use different styles
           ),
       // Show onboarding screen only once
-      initialRoute: initScreen == 0 || initScreen == null ? "first" : "/",
+      initialRoute: initScreen == 0 || initScreen == null ? "onBoarding" : "",
+//      home: _isLoggedIn ? Home() : Menu(),
       routes: {
-        '/': (context) => Home(),
-        "first": (context) => OnboardingScreen(),
+        'home': (context) => Home(),
+        "onBoarding": (context) => OnboardingScreen(),
+        "menu": (context) => Menu(),
       },
     );
   }
